5db956fc7fb3802b2e5b93ab45bb42bf1798abda
Kaif <125804924+mkr-infinity@users.noreply.github.com>
2026-03-13 02:12:39 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.mkr.revisionmaster',
-  appName: 'Revision Master',
-  webDir: 'dist'
-};
-
-export default config;
